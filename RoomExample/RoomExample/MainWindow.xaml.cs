cce92947a3094f076b9d4b657d6fe33cdfac4bc7
IvleevaViktoria <118309123+IvleevaViktoria@users.noreply.github.com>
2023-03-09 13:42:21 +0300

    Add files via upload

---
+++ RoomExample/RoomExample/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using RoomLibrary1;
+
+namespace RoomExample
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Room room1 = new Room();
+        Room room2 = new Room();
+        Random rnd = new Random();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void ButtonOpen1_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonOpen2.IsEnabled = true;
+            room1.RoomLength = rnd.Next(2, 11);
+            room1.RoomWidth = rnd.Next(2, 11);
+            int numP = rnd.Next(1, 9);
+
+            LabelLength1.Content = room1.RoomLength;
+            LabelWidth1.Content = room1.RoomWidth;
+            LabelNumPerson1.Content =numP;
+
+            LabelPerimetr1.Content = room1.RoomPerimetr();
+            LabelArea1.Content = room1.RoomArea();
+            LabelPersonArea1.Content =Math.Round( room1.PersonArea(numP),3  );
+
+
+        }
+        /// <summary>
+        /// Открытие второй комнаты
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ButtonOpen2_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonAll.IsEnabled = true;
+
+            room2.RoomLength = Convert.ToDouble(TBLength2.Text);
+            room2.RoomWidth = Convert.ToDouble(TBWidth2.Text);
+            int numP = Convert.ToInt32(TBNumPerson2.Text);
+
+            LabelPerimetr2.Content = room2.RoomPerimetr();
+            LabelArea2.Content = room2.RoomArea();
+            LabelPersonArea2.Content = Math.Round(room2.PersonArea(numP), 3);
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void ButtonAll_Click(object sender, RoutedEventArgs e)
+        {
+            LabelAllArea.Content = room1.RoomArea() + room2.RoomArea();
+        }
+    }
+}
---
+++ RoomExample/RoomExample/Window1.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using RoomLibrary1;
+
+namespace RoomExample
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        Room room = new Room();
+        LivingRoom livingRoom= new LivingRoom();
+        Office office= new Office();
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ListRooms.Content = "";
+            ListRooms.Content += room.Info() + "\n";
+            ListRooms.Content += office.Info();
+
+        }
+
+        private void BAddRoom_Click(object sender, RoutedEventArgs e)
+        {
+            room.RoomLength = Convert.ToDouble(TBLengthR.Text);
+            room.RoomWidth = Convert.ToDouble(TBWidthR.Text);
+        }
+
+        private void BAddLivingRoom_Click(object sender, RoutedEventArgs e)
+        {
+            livingRoom.RoomLength = Convert.ToDouble(TBLengthL.Text);
+            livingRoom.RoomWidth = Convert.ToDouble(TBWidthL.Text);
+            livingRoom.NumWin = Convert.ToInt32(TBNumW.Text);
+        }
+
+        private void BAddOffice_Click(object sender, RoutedEventArgs e)
+        {
+            office.RoomLength = Convert.ToDouble(TBLengthO.Text);
+            office.RoomWidth = Convert.ToDouble(TBWidthO.Text);
+            office.NumSockets = Convert.ToInt32(TBNumS.Text);
+        }
+    }
+}
